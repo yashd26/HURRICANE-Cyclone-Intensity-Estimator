9c23f6339dddfac9b2b5a5eeb020dae9f8b1f7f3
Mr.mad <yashd026@gmail.com>
2022-02-07 17:17:25 +0530

    adding model fields(NOT FINAL)

--- intensity_app/admin.py
+++ intensity_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CycloneData
 
 # Register your models here.
+admin.site.register(CycloneData)
--- intensity_app/models.py
+++ intensity_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CycloneData(models.Model):
+    category_choices = [
+        (1, 'Category 1'),
+        (2, 'Category 2'),
+        (3, 'Category 3'),
+        (4, 'Category 4'),
+        (5, 'Category 5'),
+    ]
+
+    cyclone_name = models.CharField(max_length=50, help_text='Enter cyclone name', null=True, blank=True)
+    intensity = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    category = models.IntegerField(choices=category_choices, default=1)
+    latitude = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    origin_date_time = models.DateTimeField(null=True, blank=True)
