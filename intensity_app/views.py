b0dcd10c72c2eaf408e16a856399565007d1d592
Mr.mad <yashd026@gmail.com>
2022-02-09 19:07:50 +0530

    Added initial backend code.

--- intensity_app/views.py
+++ intensity_app/views.py
@@ -1,10 +1,64 @@
-from django.shortcuts import render 
+from django.shortcuts import render ,redirect
+from .models import StormData
 
-def home(request): 
-    return render(request,'intensity_app/home.html')
+def home(request):
+    return render(request, 'intensity_app/home.html')
 
-def explore(request): 
+def explore_all(request):
+    print("explore_all")
+    context = {
+        'storms' : StormData.objects.all(),
+    }    
+    return render(request, 'intensity_app/explore.html' , context)
+
+def explore(request):
     return render(request,'intensity_app/explore.html')
 
 def readcontent(request): 
     return render(request,'intensity_app/readcontent.html')
+
+def about(request):
+    return render(request, 'intensity_app/about.html')
+
+def explore_by_name(request, name):
+    storms = StormData.objects.filter(storm_name=name)
+    context = {
+        "storms" : storms,
+    }
+
+    return render(request,'intensity_app/explore.html', context=context)
+
+def explore_by_year(request, year):
+    storms = StormData.objects.filter(origin_date__year=year)
+    context = {
+        "storms" : storms,
+    }
+
+    return render(request,'intensity_app/explore.html', context=context)
+
+def explore_by_month(request, year, month):
+    storms = StormData.objects.filter(origin_date__year=year, 
+        origin_date__month=month)
+    context = {
+        "storms" : storms,
+    }
+
+    return render(request,'intensity_app/explore.html', context=context)
+
+def explore_by_date(request, year, month, date):
+    storms = StormData.objects.filter(origin_date__year=year, 
+        origin_date__month=month, 
+        origin_date__day=date)
+    context = {
+        "storms" : storms,
+    }
+
+    return render(request,'intensity_app/explore.html', context=context)
+
+def storm_detail(request):
+    storms = StormData.objects.filter(storm_id=request.GET['id'])
+    context = {
+        "storms" : storms,
+    }
+
+    return render(request,'intensity_app/explore.html', context=context)
--- project1/urls.py
+++ project1/urls.py
@@ -15,17 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('intensity_app.urls')),
-]
-
-urlpatterns += [
-    path('', RedirectView.as_view(url='home/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
