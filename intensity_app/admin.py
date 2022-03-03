3a7af4c67fae309a8814b538e8c52ac831298aa8
PriyanshuPatil007 <priyanshupatil806>
2022-03-03 08:30:36 +0530

    Admin model added

--- intensity_app/admin.py
+++ intensity_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import StormData
+from .models import StormTrack
 
 # Register your models here.
 admin.site.register(StormData)
+admin.site.register(StormTrack)
