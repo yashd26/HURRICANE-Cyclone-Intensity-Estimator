24522352555bb83ee06a335d973d6f449c32046f
yashd26 <yashd026@gmail.com>
2023-04-06 00:58:41 +0530

    just get deployed

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
